b9f76735bea0e874645c96c5e2d29ee58aeedeff
Aman Singh <amansingh@Amans-MacBook-Air.local>
2021-10-08 21:08:43 +0530

    first commit

---
+++ lib/palette.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class Palette {
+  static const Color background = Color(0xff040404);
+  static const Color button = Color(0xff238B46);
+  static const Color text = Color(0xffEDEDED);
+  static const Color genderContainer = Color(0xff80807D);
+}
